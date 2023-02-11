04c1a4872fd54f7366fe7434d8cd6ae7d5b12bb8
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-02-11 23:59:18 +0000

    :sparkles: First commit

---
+++ hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1.d
@@ -0,0 +1,5 @@
+/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1: src/main.rs
+
+/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/belchior/projects/personal/rust-basics/hello_world/target/debug/hello_world: /home/belchior/projects/personal/rust-basics/hello_world/src/main.rs
