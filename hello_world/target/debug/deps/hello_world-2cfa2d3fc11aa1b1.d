867c331680f5b96e207810226df72b765ff4465b
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-02-12 00:02:00 +0000

    Removed target dir

--- hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1.d
+++
@@ -1,5 +0,0 @@
-/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1: src/main.rs
-
-/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-2cfa2d3fc11aa1b1.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-dc812c5d4230b9dc.d
+++
@@ -1,5 +0,0 @@
-/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-dc812c5d4230b9dc.rmeta: src/main.rs
-
-/home/belchior/projects/personal/rust-basics/hello_world/target/debug/deps/hello_world-dc812c5d4230b9dc.d: src/main.rs
-
-src/main.rs:
